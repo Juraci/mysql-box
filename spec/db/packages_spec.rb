c543d19f8e827a2b5855360180d7f2b5cd27ce68
Juraci <juraci.vieira@gmail.com>
2015-08-20 16:00:32 -0300

    adding pip package shared examples

--- spec/db/packages_spec.rb
+++ spec/db/packages_spec.rb
@@ -8,6 +8,15 @@ shared_examples 'a required package' do |packages|
   end
 end
 
+shared_examples 'a required pip package' do |pip_packages|
+  pip_packages.each do |pip_package|
+    describe command("pip list | grep #{pip_package}") do
+      its(:stdout) { should match /#{pip_package}/  }
+    end
+  end
+end
+
 describe 'dependencies' do
   it_behaves_like 'a required package', ['mysql-server', 'python-dev', 'libmysqlclient-dev', 'python-pip', 'build-essential']
+  it_behaves_like 'a required pip package', ['MySQL-python']
 end
